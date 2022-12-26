690d88c2395466a7a576e2c728fe39825fedfe53
williamchew85 <williamchew85@gmail.com>
2022-12-26 16:39:29 +0800

    v2
    This script defines a new method called notify_on_arrival in the BusArrivalAPI class. This method takes three arguments: the bus stop code, the bus number, and the desired arrival time. It then enters an infinite loop that makes repeated requests to the LTA DataMall API to get the estimated arrival time for the specified bus at the specified bus stop. If the estimated arrival time matches the desired arrival time, the loop breaks and a notification is printed to the console.

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
-import requests
 import time
+import requests
 
-# Set your location and bus stop code
-latitude = 1.3521
-longitude = 103.8198
-bus_stop_code = '04111'
+class BusArrivalAPI:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = "http://datamall2.mytransport.sg/ltaodataservice/"
 
-# Set the number of minutes before arrival to send notification
-notification_minutes = 10
+    def get_bus_arrival(self, bus_stop_code, bus_number):
+        headers = {
+            "AccountKey": self.api_key,
+            "accept": "application/json"
+        }
+        url = f"{self.base_url}BusArrivalv2?BusStopCode={bus_stop_code}&ServiceNo={bus_number}"
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Request failed with status code {response.status_code}")
 
-while True:
-    # Make a request to the bus arrival API
-    api_url = 'http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2'
-    headers = {'AccountKey': 'YOUR_API_KEY', 'accept': 'application/json'}
-    params = {'BusStopCode': bus_stop_code}
-    response = requests.get(api_url, headers=headers, params=params)
-    data = response.json()
+    def notify_on_arrival(self, bus_stop_code, bus_number, arrival_time):
+        while True:
+            arrival_data = self.get_bus_arrival(bus_stop_code, bus_number)
+            bus_arrival = arrival_data["Services"][0]["NextBus"]["EstimatedArrival"]
+            if bus_arrival == arrival_time:
+                print("Your bus is about to arrive!")
+                break
+            time.sleep(10)
 
-    # Loop through the arrival times for each bus
-    for bus in data['Services']:
-        for arrival in bus['NextBus']['EstimatedArrival']:
-            # Calculate the time until arrival in minutes
-            arrival_time = arrival['EstimatedArrival']
-            arrival_minutes = (arrival_time - time.time()) / 60
-            if arrival_minutes <= notification_minutes:
-                # Send the notification
-                message = f'{bus["ServiceNo"]} is arriving at {bus_stop_code} in {arrival_minutes:.0f} minutes'
-                send_notification(message)
-    
-    # Wait for the next update
-    time.sleep(60)
+# Create an instance of the BusArrivalAPI class
+api = BusArrivalAPI("your_api_key_here")
 
-def send_notification(message):
-    # Replace this with code to send the notification using your preferred method
-    print(message)
+# Set the desired bus stop code and bus number
+bus_stop_code = "12345"
+bus_number = "123"
+
+# Set the desired arrival time (in the format "YYYY-MM-DDTHH:MM:SS+08:00")
+arrival_time = "2022-12-22T12:00:00+08:00"
+
+# Start the notification loop
+api.notify_on_arrival(bus_stop_code, bus_number, arrival_time)
